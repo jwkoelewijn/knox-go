85ea6509c1bc29eb6863653f2b46910c653b7cf4
J.W. Koelewijn <jwkoelewijn@gmail.com>
2015-08-22 01:28:09 +0200

    Changed test name and moved it to another file

--- document_list_test.go
+++ document_list_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
 
 func TestCost(t *testing.T) {
 	testCases := []testCase{
@@ -23,4 +27,37 @@ func TestCost(t *testing.T) {
 	if cost := docList.Cost(); cost != 713+1980 {
 		t.Errorf("Expected the cost to be %d, not %d", 713+1980, cost)
 	}
+}
+
+func TestSorting(t *testing.T) {
+	docList := NewDocumentList()
+
+	size := 10
+	for i := 1; i <= size; i += 1 {
+		value := rand.Intn(size)
+		docList = docList.Add(Document{Name: fmt.Sprintf("Document %d", 10-i), Size: i, Value: value, SecrecyRatio: float64(value) / float64(i)})
+	}
+
+	fmt.Println("docList:")
+	fmt.Println(docList)
+
+	docList.SortByName()
+	fmt.Println("docList (by Name):")
+	fmt.Println(docList)
+
+	docList.SortBySize()
+	fmt.Println("docList (by Size):")
+	fmt.Println(docList)
+
+	docList.SortByDescendingValue()
+	fmt.Println("docList (by Descending Value):")
+	fmt.Println(docList)
+
+	docList.SortByRatio()
+	fmt.Println("docList: (by Ratio)")
+	fmt.Println(docList)
+
+	docList.SortByDescendingRatio()
+	fmt.Println("docList: (by Descending Ratio)")
+	fmt.Println(docList)
 }
--- document_test.go
+++ document_test.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"testing"
-)
+import "testing"
 
 type testCase struct {
 	input   string
@@ -53,37 +49,4 @@ func TestUnmarshal(t *testing.T) {
 			}
 		}
 	}
-}
-
-func TestCombinations(t *testing.T) {
-	docList := NewDocumentList()
-
-	size := 10
-	for i := 1; i <= size; i += 1 {
-		value := rand.Intn(size)
-		docList = docList.Add(Document{Name: fmt.Sprintf("Document %d", 10-i), Size: i, Value: value, SecrecyRatio: float64(value) / float64(i)})
-	}
-
-	fmt.Println("docList:")
-	fmt.Println(docList)
-
-	docList.SortByName()
-	fmt.Println("docList (by Name):")
-	fmt.Println(docList)
-
-	docList.SortBySize()
-	fmt.Println("docList (by Size):")
-	fmt.Println(docList)
-
-	docList.SortByDescendingValue()
-	fmt.Println("docList (by Descending Value):")
-	fmt.Println(docList)
-
-	docList.SortByRatio()
-	fmt.Println("docList: (by Ratio)")
-	fmt.Println(docList)
-
-	docList.SortByDescendingRatio()
-	fmt.Println("docList: (by Descending Ratio)")
-	fmt.Println(docList)
 }
